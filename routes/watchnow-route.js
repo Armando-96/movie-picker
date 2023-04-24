63c5c4b83e183783ab6d59d1b85da9691edd97ff
Armando Nardelli <armando.nardelli96@gmail.com>
2023-04-25 00:33:39 +0200

    Aggiunte api per numero like e film piaciuti Sorto bug con redirect, da risolvere.

--- routes/watchnow-route.js
+++ routes/watchnow-route.js
@@ -3,7 +3,7 @@ const sessionController = require("../controllers/session-controllers.js");
 const whatchNowController = require("../controllers/watchnow-controllers.js");
 const watchNowRoute = express.Router();
 
-watchNowRoute.get("/getLike", whatchNowController.getNumLikeInSession);
+watchNowRoute.get("/getNumLike", whatchNowController.getNumLikeInSession);
 watchNowRoute.get("/getLikeMovies", whatchNowController.getLikeMovies);
 
 module.exports = watchNowRoute;
