af4af91037a7962dfba5f3477a40d106b3c786eb
Armando Nardelli <armando.nardelli96@gmail.com>
2023-04-24 23:13:08 +0200

    Inizio implementazione fullstack watchnow (creati i file pug components)

--- routes/session-route.js
+++ routes/session-route.js
@@ -6,5 +6,7 @@ sessionRoute.get("/next", sessionController.addInteractionOld);
 sessionRoute.get("/addInteraction", sessionController.addInteraction);
 sessionRoute.get("/end", sessionController.endSession);
 sessionRoute.get("/", sessionController.chooseMod);
+//sessionRoute.get("/torneo", sessionController.torneo);
+//sessionRoute.get("/torneo/list", sessionController.torneo); // restituisce al chiamante un file JSON contenente l'array dei movie_id da mostrare
 
 module.exports = sessionRoute;
