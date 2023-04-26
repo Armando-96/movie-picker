01cd3c2445814296c64c2309379d72bd52315295
Armando Nardelli <armando.nardelli96@gmail.com>
2023-04-26 19:20:09 +0200

    Aggiornamento pagina sincrono,rimosso localStorage localStorage sostituito con variabili

--- public/js/discovery-initial.js
+++ public/js/discovery-initial.js
@@ -1,23 +1,23 @@
 function addInteraction(preference_choice) {
-    movie_id = localStorage.getItem("movie_id");
     preference = preference_choice;
-    $.get("/session/next" + '?' + 'preference=' + preference + '&' + 'movie_id=' + movie_id, function (data, status) {
-        if (status == "success") {
+    $.get({
+        async: false,
+        url: "/session/next" + '?' + 'preference=' + preference + '&' + 'movie_id=' + movie_id,
+        success: function (data) {
             if (data.nonext) {
                 $("#body").html('<h1>Non ci sono più film da mostrare</h1>');
             } else if (preference_choice == "selected") {
                 $("#body").html('<h1>Film selezionato</h1>');//In attesa di sviluppo, da inserire una pagina di ringraziamenti o qualcosa del genere
                 return;
             } else {
-                localStorage.setItem("movie_id", data.id);
-                $("#title").html(data.original_title);
+                movie_id = data.id;
+                $("#title").html(data.title);
                 $("#rating").html(data.vote_average);
                 $("#overview").html(data.overview);
-                $("#poster").attr("src", localStorage.getItem("prefix_poster_path") + data.poster_path);
+                $("#poster").attr("src", prefix_poster_path + data.poster_path);
             }
-        } else {
-            alert("Errore nella richiesta");
-        }
+        },
+        error: function () { alert("Errore nella richiesta"); }
     });
 }
 
--- public/js/watchnow-initial.js
+++ public/js/watchnow-initial.js
@@ -1,35 +1,44 @@
 function addInteraction(preference_choice) {
-    movie_id = localStorage.getItem("movie_id");
     preference = preference_choice;
-    $.get("/session/next" + '?' + 'preference=' + preference + '&' + 'movie_id=' + movie_id, function (data, status) {
-        if (status == "success") {
+    $.get({
+        async: false,
+        url: "/session/next" + '?' + 'preference=' + preference + '&' + 'movie_id=' + movie_id,
+        success: function (data) {
             if (data.nonext) {
                 $("#body").html('<h1>Non ci sono più film da mostrare</h1>');
             } else if (preference_choice == "selected") {
                 $("#body").html('<h1>Film selezionato</h1>');//In attesa di sviluppo, da inserire una pagina di ringraziamenti o qualcosa del genere
                 return;
             } else {
-                localStorage.setItem("movie_id", data.id);
-                $("#title").html(data.original_title);
+                movie_id = data.id;
+                $("#title").html(data.title);
                 $("#rating").html(data.vote_average);
                 $("#overview").html(data.overview);
-                $("#poster").attr("src", localStorage.getItem("prefix_poster_path") + data.poster_path);
+                $("#poster").attr("src", prefix_poster_path + data.poster_path);
             }
-        } else {
-            alert("Errore nella richiesta");
-        }
+        },
+        error: function () { alert("Errore nella richiesta"); }
     });
     if (preference_choice === "like") {
-        $.get("/watchnow/getNumLike", function (data, status) {
-            if (status == "success") {
+        $.get({
+            async: false,
+            url: "/watchnow/getNumLike",
+            success: function (data) {
                 json = JSON.parse(data);
                 json.n_likes++;
                 if (json.n_likes == 16) {
-                    $("#body").html('<h1>Hai raggiunto 16 film piaciuti -> Torneo!</h1>');//In attesa dello sviluppo del torneo
+                    $.get("/watchnow/getLikeMovies", function (data, status) {
+                        if (status == "success") {
+                            $("#body").html("<h1>Film scelti:</h1><hr>" + data);//In attesa dello sviluppo del torneo
+                        } else {
+                            alert("Errore nella richiesta di getLikeMovies");
+                        }
+                    });
                     return;
                 }
                 $("#cumulator").text(json.n_likes + "/16");
-            } else {
+            },
+            error: function () {
                 alert("Errore nella richiesta di getNumLike");
             }
         });
